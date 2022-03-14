2619205df4c62695fe9889862d1ff77f08509d23
Your Name <you@example.com>
2022-03-14 18:58:06 +0300

    add b

--- src/logur.c
+++ src/logur.c
@@ -1,14 +1,8 @@
-#include <stdlib.h>
-
 #include <logur.h>
 
 typedef struct logur_t {
-	char*	log_file;
-	int		log_level;		
+  char *log_file;
+  int log_level;
 } logur_t;
 
-logur_t* logur_init() {
-	return (logur_t*)malloc(sizeof(logur_t));
-}
-
-
+logur_t *logur_init() { return (logur_t *)malloc(sizeof(logur_t)); }
