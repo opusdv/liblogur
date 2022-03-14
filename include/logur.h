f0263b128d45e878459c498dbadc15d264ee3e7e
Your Name <you@example.com>
2022-03-14 19:30:24 +0300

    change

--- include/logur.h
+++ include/logur.h
@@ -4,7 +4,7 @@
  * This file is part of Logur.                                              *                                 
  *                                                                          *                                 
  *   Logus is free software: you can redistribute it and/or modify it       *                                 
- *   under the terms of the GNU Lesser General Public License as published  *                                 
+ *   under the terms of the GNU General Public License as published  *                                 
  *   by the Free Software Foundation, either version 3 of the License, or   *                                 
  *   (at your option) any later version.                                    *                                 
  *                                                                          *                                 
@@ -13,7 +13,7 @@
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *                                 
  *   GNU Lesser General Public License for more details.                    *                                 
  *                                                                          *                                 
- *   You should have received a copy of the GNU Lesser General Public       *                                 
+ *   You should have received a copy of the GNU General Public				*                                 
  *   License along with Box.  If not, see <http://www.gnu.org/licenses/>.   *                                 
  ****************************************************************************/ 
 
