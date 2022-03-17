10aeb9c9e39563dae73b1397f92a7bdd23ff221e
Your Name <you@example.com>
2022-03-17 20:30:48 +0300

    lint

--- include/logur.h
+++ include/logur.h
@@ -1,20 +1,20 @@
-/****************************************************************************                                 
- * Copyright (C) 2022 by Donatas Vaytukaytis                                *                                 
- *                                                                          *                                 
- * This file is part of Logur.                                              *                                 
- *                                                                          *                                 
- *   Logus is free software: you can redistribute it and/or modify it       *                                 
- *   under the terms of the GNU General Public License as published  *                                 
- *   by the Free Software Foundation, either version 3 of the License, or   *                                 
- *   (at your option) any later version.                                    *                                 
- *                                                                          *                                 
- *   Logur is distributed in the hope that it will be useful,               *                                 
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *                                 
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *                                 
- *   GNU Lesser General Public License for more details.                    *                                 
- *                                                                          *                                 
- *   You should have received a copy of the GNU General Public				*                                 
- *   License along with Box.  If not, see <http://www.gnu.org/licenses/>.   *                                 
+/****************************************************************************
+ * Copyright (C) 2022 by Donatas Vaytukaytis                                *
+ *                                                                          *
+ * This file is part of Logur.                                              *
+ *                                                                          *
+ *   Logus is free software: you can redistribute it and/or modify it       *
+ *   under the terms of the GNU General Public License as published  *
+ *   by the Free Software Foundation, either version 3 of the License, or   *
+ *   (at your option) any later version.                                    *
+ *                                                                          *
+ *   Logur is distributed in the hope that it will be useful,               *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *   GNU Lesser General Public License for more details.                    *
+ *                                                                          *
+ *   You should have received a copy of the GNU General Public
+ ** License along with Box.  If not, see <http://www.gnu.org/licenses/>.   *
  ****************************************************************************/
 
 #ifndef _LOGUR_H_
